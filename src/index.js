8bbac41dc8e428e2a0e86fd8b615f0d3ed071e0a
alihamza-doc <alihamza141250@gmail.com>
2025-05-15 07:17:20 -0700

    db connected

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+import { DB_NAME } from "./constants";
+
+import express from "express"
+const app = express()
+
+( async ()=>{
+    try{
+     await mongoose.connect(`${process.env.MONGODB-URI}/${DB_NAME}`) 
+     app.on("error", (error)=>{
+      console.log("ERROR:", error)
+      throw error
+     })
+    }
+
+     catch(error){
+        console.error("ERROR", error)
+        throw err
+     }
+
+})()
